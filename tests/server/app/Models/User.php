455d62cd3abf08591f095be934c7e506ee493877
mrk-j <mrk-j@users.noreply.github.com>
2022-09-06 18:51:59 +0000

    Fix styling

--- tests/server/app/Models/User.php
+++ tests/server/app/Models/User.php
@@ -11,7 +11,9 @@ use Laravel\Lumen\Auth\Authorizable;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable, HasFactory;
+    use Authenticatable;
+    use Authorizable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
